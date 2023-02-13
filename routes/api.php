3d6a996bd4f85ea0c9ba1e89fb260b9f78a9e505
Rizki <104330887+MRizki28@users.noreply.github.com>
2023-02-13 22:01:04 +0800

    configurate jwt

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\BarangController;
+use App\Http\Controllers\Auth\JWTController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,12 +15,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('/jwt',[JWTController::class, 'index']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['Middleware'=> 'api'], function($router){
+    Route::post('/register', [JWTController::class, 'register']);
+    Route::post('/login', [JWTController::class, 'login']);
+    Route::post('/refresh', [JWTController::class, 'refresh']);
+
+});
+
+
 
 Route::get('/barang', [BarangController::class, 'index']);
 Route::post('/barang', [BarangController::class, 'store']);
 Route::put('/barang/edit/{id}', [BarangController::class, 'update']);
 Route::get('/barang/{id}' , [BarangController::class, 'show']);
+
